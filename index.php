80847fec273940f2d590bb3ee14fcfcc73439c20
alessandrocapanna <acapanna75@gmail.com>
2020-08-26 14:32:15 +0200

    commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+  class user
+  {
+
+    public $ID;
+    public $nome;
+    public $cognome;
+    public $email;
+    public $età;
+
+    public function getFullName($nome , $cognome){
+      echo 'Utente: ' . $nome . ' ' . $cognome . '<br>';
+    }
+  }
+
+  $utenti = [
+    [
+      'ID' => 1,
+      'nome' => 'alessandro',
+      'cognome' => 'rossi',
+      'email' => 'alessandrorossi@hotmail.it',
+      'età' => 29
+    ],
+    [
+      'ID' => 2,
+      'nome' => 'daniele',
+      'cognome' => 'bianchi',
+      'email' => 'danielebianchi@hotmail.it',
+      'età' => 22
+    ],
+    [
+      'ID' => 2,
+      'nome' => 'mirko',
+      'cognome' => 'scopano',
+      'email' => 'mirkoscopano@hotmail.it',
+      'età' => 55
+    ],
+  ];
+
+  foreach ($utenti as $utente) {
+    $userDaStampare = new user();
+    $userDaStampare->ID = $utente['ID'];
+    $userDaStampare->nome = $utente['nome'];
+    $userDaStampare->cognome = $utente['cognome'];
+    $userDaStampare->email = $utente['email'];
+    $userDaStampare->età = $utente['età'];
+    $userDaStampare->getFullName($userDaStampare->nome , $userDaStampare->cognome);
+  }
+?>
